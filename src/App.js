a3ecf96350ad3d5680b19316d09bf4ee7c5b18a9
snprabu <sn.prabu@gmail.com>
2018-12-07 18:50:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
 import Aside from './components/Aside';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (     
       <div className="App">
-        {logo}
         <Header />        
         < Aside />
         < Main />
